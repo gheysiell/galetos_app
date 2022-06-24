d7a272bf629eda62ebf340648f58b5487e405a1f
gheysiell <gheysiellfarukzinn@hotmail.com>
2022-06-24 16:50:16 -0400

    release melhories in the structure

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -4,96 +4,97 @@ import "../../assets/css/home.css"
 import { Header } from '../../components/Header/'
 import { LeftMain } from '../../components/LeftMain/'
 import { Content } from '../../components/Content/'
+import { Container } from '../../components/Container'
 
 export const Home = () => {
-   const [iconDaVez, setIconDaVez] = useState("RiHome3Line")
-   const [cor, setCor] = useState("#fff")   
-   const [whatClassMain, setWhatClassMain] = useState("little")
-   const navigate = useNavigate()
+    const [iconDaVez, setIconDaVez] = useState("RiHome3Line")
+    const [cor, setCor] = useState("#fff")   
+    const [whatClassMain, setWhatClassMain] = useState("little")
+    const navigate = useNavigate()
 
-   const changeColorActive = (optionClicked) => {
-      if (optionClicked === "RiHome3Line") {
-         alert("RiHome3Line")
-         setCor("#08D6A3")
-         setIconDaVez("RiHome3Line")
-      } else if (optionClicked === "RiCouponLine") {
-         alert("RiCouponLine")
-         setCor("#08D6A3")
-         setIconDaVez("RiCouponLine")
-      }
-   }
+    const changeColorActive = (optionClicked) => {
+        if (optionClicked === "RiHome3Line") {
+            alert("RiHome3Line")
+            setCor("#08D6A3")
+            setIconDaVez("RiHome3Line")
+        } else if (optionClicked === "RiCouponLine") {
+            alert("RiCouponLine")
+            setCor("#08D6A3")
+            setIconDaVez("RiCouponLine")
+        }
+    }
 
-   const setWidthLeftMain = () => {      
-      whatClassMain === "little" ? setWhatClassMain("big") : setWhatClassMain("little")
-   }
+    const setWidthLeftMain = () => {      
+        whatClassMain === "little" ? setWhatClassMain("big") : setWhatClassMain("little")
+    }
 
-   const logOut = () => {
-      navigate('/')
-   }
+    const logOut = () => {
+        navigate('/')
+    }
 
-   return (
-      <div className="container">
-         <LeftMain 
-            whatClassMain={whatClassMain} 
-            setWidthLeftMain={() => setWidthLeftMain()}
-            changeColorActive={() => changeColorActive()} 
-            logOut={() => logOut()}
-            iconDaVez={iconDaVez}
-            cor={cor}
-            navigate={navigate}
-         />
-         <Header 
-            whatClassMain={whatClassMain}
-            setWidthLeftMain={() => setWidthLeftMain()}
-         />
-         <Content
-            whatClassMain={whatClassMain}
-         >
-            <div className="statistics-panel">
-               <div 
-                  className="statistics-squares"
-                  style={{background: "#FA8072"}}
-               >
-                  <div  className={"statistics-squares-titles"}>
-                     <h5>Total de itens vendidos</h5>
-                  </div>
-                  <div className={"statistics-squares-values"}>
-                     <h1>37</h1>
-                  </div>                  
-               </div>
-               <div
-                  className="statistics-squares"
-                  style={{background: "#923effbe"}}
-               >
-                  <div className={"statistics-squares-titles"}>
-                     <h5 >Total de valor</h5>
-                  </div>
-                  <div className={"statistics-squares-values"}>
-                     <h1>23</h1>
-                  </div>                  
-               </div>
-               <div 
-                  className="statistics-squares"
-                  style={{background: "#F4A460"}}
-               >
-                  <div className={"statistics-squares-titles"}>
-                     <h5>Porcentagem de vendas batida</h5>
-                  </div>
-                  <div className={"statistics-squares-values"}>
-                     <h1>06</h1>
-                  </div>
-               </div>
-               <div
-                  className="statistics-squares"
-                  style={{background: "#17a7d3f1"}}
-               >                  
-                  <h5 className={"statistics-squares-titles"}>Produtos em estoque</h5>
-                  <div className={"statistics-squares-values"}>
-                     <h1>17</h1>
-                  </div>
-               </div>
-            </div>
-         </Content>
-      </div>      
-   )
+    return (
+        <Container>
+            <LeftMain 
+                whatClassMain={whatClassMain} 
+                setWidthLeftMain={() => setWidthLeftMain()}
+                changeColorActive={() => changeColorActive()} 
+                logOut={() => logOut()}
+                iconDaVez={iconDaVez}
+                cor={cor}
+                navigate={navigate}
+            />
+            <Header 
+                whatClassMain={whatClassMain}
+                setWidthLeftMain={() => setWidthLeftMain()}
+            />
+            <Content
+                whatClassMain={whatClassMain}
+            >
+                <div className="statistics-panel">
+                    <div 
+                        className="statistics-squares"
+                        style={{background: "#FA8072"}}
+                    >
+                        <div  className={"statistics-squares-titles"}>
+                            <h5>Total de itens vendidos</h5>
+                        </div>
+                        <div className={"statistics-squares-values"}>
+                            <h1>37</h1>
+                        </div>                  
+                    </div>
+                    <div
+                        className="statistics-squares"
+                        style={{background: "#923effbe"}}
+                    >
+                        <div className={"statistics-squares-titles"}>
+                            <h5 >Total de valor</h5>
+                        </div>
+                        <div className={"statistics-squares-values"}>
+                            <h1>23</h1>
+                        </div>                  
+                    </div>
+                    <div 
+                        className="statistics-squares"
+                        style={{background: "#F4A460"}}
+                    >
+                        <div className={"statistics-squares-titles"}>
+                            <h5>Porcentagem de vendas batida</h5>
+                        </div>
+                        <div className={"statistics-squares-values"}>
+                            <h1>06</h1>
+                        </div>
+                    </div>
+                    <div
+                        className="statistics-squares"
+                        style={{background: "#17a7d3f1"}}
+                    >                  
+                        <h5 className={"statistics-squares-titles"}>Produtos em estoque</h5>
+                        <div className={"statistics-squares-values"}>
+                            <h1>17</h1>
+                        </div>
+                    </div>
+                </div>
+            </Content>
+        </Container>
+    )
 }
--- client/src/pages/Pedidos/index.jsx
+++ client/src/pages/Pedidos/index.jsx
@@ -6,6 +6,7 @@ import { Content } from "../../components/Content"
 import { SearchBox } from '../../components/SearchBox'
 import { ButtonNovo } from '../../components/ButtonNovo'
 import { ContentHeaderBody } from '../../components/ContentHeaderBody'
+import { Container } from '../../components/Container'
 import { BiEdit } from 'react-icons/bi'
 import { BiTrash } from 'react-icons/bi'
 import "../../assets/css/pedidos.css"
@@ -16,11 +17,21 @@ export const Pedidos = (props) => {
     const [whatClassMain, setWhatClassMain] = useState("big")
     const navigate = useNavigate()
     const pedidos = [
-        {id: "1", description: "Arroz", quantidade: "3", preco: 25.56},
-        {id: "2", description: "Feijao", quantidade: "2", preco: 18.94},
-        {id: "3", description: "Frango", quantidade: "3", preco: 45.05},
-        {id: "4", description: "Cupim", quantidade: "5", preco: 12.52},
-        {id: "5", description: "Costela", quantidade: "1", preco: 19.90}
+        {id: "1", description: "Arroz", quantidade: 3, preco: 25.56},
+        {id: "2", description: "Feijao", quantidade: 2, preco: 18.94},
+        {id: "3", description: "Frango", quantidade: 3, preco: 45.05},
+        {id: "4", description: "Cupim", quantidade: 5, preco: 12.52},
+        {id: "5", description: "Costela", quantidade: 1, preco: 19.90},
+        {id: "6", description: "Galetos", quantidade: 1, preco: 78.93},
+        {id: "7", description: "Coxinha", quantidade: 3, preco: 43.12},
+        {id: "8", description: "Coca-cola", quantidade: 2, preco: 54.12},
+        {id: "9", description: "Fanta", quantidade: 1, preco: 60.52},
+        {id: "10", description: "Guarana", quantidade: 4, preco: 15.21},
+        {id: "11", description: "Dydyo", quantidade: 7, preco: 18.43},
+        {id: "11", description: "Dydyo", quantidade: 7, preco: 18.43},
+        {id: "11", description: "Dydyo", quantidade: 7, preco: 18.43},
+        {id: "11", description: "Dydyo", quantidade: 7, preco: 18.43},
+        {id: "11", description: "Dydyo", quantidade: 7, preco: 18.43}
     ]
     const pedidosList = pedidos.map((pedido) => {
         return(
@@ -29,26 +40,20 @@ export const Pedidos = (props) => {
                 <td>{pedido.description}</td>
                 <td>{pedido.quantidade}</td>
                 <td>{pedido.preco}</td>            
-                <td>
+                <td width={"90px"}>
                     <div className='content-actions-table'>
-                        <button 
-                            style={{height: "25px", width: "25px", background: "#0D6EFD",
-                                    border: "0 none", outline: "none", borderRadius: "4px", cursor: "pointer"}}
-                        >
+                        <div className="button-list-edit">
                             <BiEdit 
                                 style={{color: "#fff"}}
-                                size="23px"
+                                size="18px"
                             />
-                        </button>                        
-                        <button
-                            style={{height: "25px", width: "25px", background: "#DC3545",
-                                    border: "0 none", outline: "none", borderRadius: "4px", cursor: "pointer"}}
-                        >
+                        </div>                        
+                        <div className="button-list-delete">
                             <BiTrash
                                 style={{color: "#fff"}}
-                                size="23px"
+                                size="18px"
                             />
-                        </button>
+                        </div>
                     </div>
                 </td>            
             </tr>
@@ -80,7 +85,7 @@ export const Pedidos = (props) => {
     })
 
     return (
-        <div className="container">
+        <Container>
             <LeftMain
                 whatClassMain={whatClassMain} 
                 setWidthLeftMain={() => setWidthLeftMain()}
@@ -101,7 +106,7 @@ export const Pedidos = (props) => {
                     <ButtonNovo/>
                     <SearchBox/>
                 </ContentHeaderBody>
-                <div className="content-table-pedido">
+                <div className='content-table-pedido'>
                     <table className="table-pedido">
                         <tr className='header-row-table-pedido'>
                             <th>ID</th>
@@ -110,10 +115,14 @@ export const Pedidos = (props) => {
                             <th>Preço</th>
                             <th>Ações</th>
                         </tr> 
-                        {pedidosList} 
+                        {pedidosList}
                     </table>
                 </div>
+                <div className='content-footer-pedido'>
+                    <div className='content-pagination'>
+                    </div>
+                </div>
             </Content>
-        </div>
+        </Container>
     )
 }
